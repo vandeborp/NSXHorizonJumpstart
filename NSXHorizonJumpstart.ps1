e7b79625cbe496643229256b7635ab0cbf68578a
Pascal van de Bor <31301129+Paikke@users.noreply.github.com>
2017-08-31 18:56:47 +0200

    Add files via upload
    Updated

--- NSXHorizonJumpstart.ps1
+++ NSXHorizonJumpstart.ps1
@@ -30,17 +30,58 @@ depending on functions will add to different modules
 
 # Settings
 $ymlFile = "horizon7_Service.yml" # Input yml file
+$logFile = "NSXHorizonJumpstart.log" # Log File location
 # End of Settings
+
+
+
+# Function Declaration
+function Write-Log 
+{ 
+    <# 
+    .SYNOPSIS 
+        This function creates or appends a line to a log file. 
+ 
+    .PARAMETER  Message 
+        The message parameter is the log message you'd like to record to the log file. 
+ 
+    #> 
+    [CmdletBinding()] 
+    param ( 
+        [Parameter(Mandatory)] 
+        [string]$Message 
+    ) 
+     
+    try 
+    { 
+        $DateTime = Get-Date -Format "dd-MM-yy HH:mm:ss"  
+        Add-Content -Value "$DateTime - $Message" -Path "log\$logFile" 
+    } 
+    catch 
+    { 
+        Write-Error $_.Exception.Message 
+    } 
+}
+
+# End Function Declaration
+
+#########
+# Run Baby Run
+#########
+
+## Init Log with current time
+Write-Log "Starting Log run $DateTime"
 
 ## Get yml file location and read the mumbling that is in there
 # Check file location
 if (!(Test-Path $ymlFile)) {
     # Exit script because required input file does not exist
+	Write-Log "$ymlFile does not exist. Exiting script"
 	throw "$ymlFile does not exist"
 }
 # End check file 
 
-
+# Close Connections
 
 
 # EOF
